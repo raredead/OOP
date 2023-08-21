d8cab2a54c6b3ba93f96d0389224247de39a0038
raredead <smahelzed@gmail.com>
2023-08-21 16:32:01 +0700

    Написал метод вывода скорости

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -6,6 +6,26 @@ using System.Threading.Tasks;
 
 namespace Learning
 {
+    class Car
+    {
+        private int speed = 0;
+        public void PrintSpeed()
+        {
+            if (speed == 0)
+            {
+                Console.WriteLine("Стоим на месте");
+            }
+            if (speed > 0)
+            {
+                Console.WriteLine($"Едем со скоростью {speed} км\\ч");
+            }
+            if (speed < 0)
+            {
+                Console.WriteLine($"Едем назад со скоростью {speed} км\\ч");
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
